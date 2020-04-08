9b6820c65eac996ee8f33fd3208838304ba5cbc0
Wataru.Kasahara <kasahara1019@gmail.com>
2020-04-08 23:27:23 +0900

    feat(@kasaharu/ng-workbox/components): Implement the spec file for components/header

--- projects/ng-workbox/components/src/lib/header/header.component.spec.ts
+++ projects/ng-workbox/components/src/lib/header/header.component.spec.ts
@@ -1,25 +1,46 @@
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HeaderHarness } from 'projects/ng-workbox/components/testing/src/public_api';
 import { HeaderComponent } from './header.component';
 
+@Component({
+  template: `
+    <ngw-header>
+      <p id="headerTitle">{{ title }}</p>
+    </ngw-header>
+  `,
+})
+class HostComponent {
+  title = 'TestComponentTitle';
+}
+
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+  let fixture: ComponentFixture<HostComponent>;
+  let hostComponent: HostComponent;
+  let loader: HarnessLoader;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ],
-    })
-    .compileComponents();
+      declarations: [HeaderComponent, HostComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture = TestBed.createComponent(HostComponent);
+    hostComponent = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create', async () => {
+    const headerHarness = await loader.getHarness(HeaderHarness);
+    expect(headerHarness).toBeTruthy();
+  });
+
+  it('get title', async () => {
+    const headerHarness = await loader.getHarness(HeaderHarness);
+    const title = await headerHarness.getTitleText();
+    expect(title).toBe(hostComponent.title);
   });
 });
