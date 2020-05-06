8bb484aae99f8bd24a46feb46a069395754abcd5
Wataru.Kasahara <kasahara1019@gmail.com>
2020-05-06 20:32:28 +0900

    yarn prettier

--- projects/ng-workbox/components/src/lib/card/card.component.ts
+++ projects/ng-workbox/components/src/lib/card/card.component.ts
@@ -7,10 +7,7 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
