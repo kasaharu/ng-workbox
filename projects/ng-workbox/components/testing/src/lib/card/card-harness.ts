c147c5fb78219f44ac61143f6021ab4a977511d3
Wataru.Kasahara <kasahara1019@gmail.com>
2020-05-09 19:46:52 +0900

    feat(@kasaharu/ng-workbox/components/testing): Implement the methods for getting title and description using ComponentHarness

--- projects/ng-workbox/components/testing/src/lib/card/card-harness.ts
+++ projects/ng-workbox/components/testing/src/lib/card/card-harness.ts
@@ -2,4 +2,22 @@ import { ComponentHarness } from '@angular/cdk/testing';
 
 export class CardHarness extends ComponentHarness {
   static hostSelector = 'ngw-card';
+
+  // tslint:disable-next-line: variable-name
+  private _cardTitleElement = this.locatorFor('.title');
+  // tslint:disable-next-line: variable-name
+  private _cardDescriptionElement = this.locatorFor('.description');
+
+  async getTitleText(): Promise<string> {
+    return (await this._cardTitleElement()).text();
+  }
+
+  async getDescriptionText(): Promise<string> {
+    // NOTE: _cardDescriptionElement は条件によって生成されていない場合があるため catch が必要
+    try {
+      return (await this._cardDescriptionElement()).text();
+    } catch {
+      return '';
+    }
+  }
 }
